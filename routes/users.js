8950604daa1560517a0e23f8f01b902cdbb91a40
Mitrea Andrei Razvan <razvanmitrea1@gmail.com>
2019-05-06 20:59:29 +0300

    remove person from json

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
@@ -12,11 +13,46 @@ router.post('/add', function(req, res, next) {
   var lastName = req.body.lastName;
   var phone = req.body.phone;
   console.warn('add', firstName, lastName, phone);
-  // TODO SAVE THIS DATA IN PERSONS.JSON
+
+  var persons = require('../public/data/persons.json')
+  //var strPersons = fs.readFileSync('./public/data/persons.json'); o alta metoda
+  //var persons = JSON.parse(strPersons); o alta metoda
+
+  persons.push({
+    firstName,
+    lastName,
+    phone
+  });
+
+  var str = JSON.stringify(persons, null, 2)
+  fs.writeFileSync('./public/data/persons.json', str);
+
+  console.log(persons);
   res.json({
     success: true,
-    message: 'TODO'
+    message: 'Done!'
+  });
+});
+
+router.delete('/delete', function(req, res, next) {
+  var id = req.body.id;
+  console.warn('remove person', id);
+
+  var persons = require('../public/data/persons.json')
+
+ var remainingPersons = persons.filter(function(person) {
+    return person.id != id;
+  })
+
+  var str = JSON.stringify(remainingPersons, null, 2)
+  fs.writeFileSync('./public/data/persons.json', str);
+
+  console.log(persons);
+  res.json({
+    success: true,
+    message: 'Done!'
   });
 });
 
 module.exports = router;
+
